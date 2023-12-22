a5b8629b56f97fa35797c56badadedaea346429e
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-22 03:37:18 +0100

    Update initialize_services.py

--- initialize_services.py
+++ initialize_services.py
@@ -1,24 +1,27 @@
 import os
 import streamlit as st
-import google.auth
-from llama_index import StorageContext, load_index_from_storage
+from llama_index.indices import VectaraIndex
+from llama_index.llms import Gemini
 from .g_key import write_file
 
 @st.cache_resource
 def initialize_services():
-    # Authenticate with Google Cloud
-    write_file()
-    key_path = st.secrets["JSON_PATH"]
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
-    credentials, project_id = google.auth.default()
+    # Authenticate with Google Gemini and OpenAI
 
-    # Authenticate with OpenAI
+    os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
     os.environ["OPENAI_API_KEY"] = st.secrets["OPEN_AI_API_KEY"]
+    llm = Gemini(
+        model="models/gemini-pro-vision"
+    )
+    
 
     # Load the vector index
-    storage_context = StorageContext.from_defaults(
-        persist_dir="ask_priya_index")
-    vector_index = load_index_from_storage(storage_context)
-    query_engine = vector_index.as_query_engine()
-
+    query_engine = index.as_query_engine(
+        llm=llm,
+        similarity_top_k=5,
+        summary_enabled=False,
+        vectara_query_mode="mmr",
+        mmr_k=50,
+        mmr_diversity_bias=0.5,
+    )
     return query_engine
