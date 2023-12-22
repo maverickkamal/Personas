9bb67f656f83af292834959a1a712379958be230
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-22 11:32:10 +0100

    Update content.py

--- content.py
+++ content.py
@@ -1,23 +1,21 @@
 import requests
-import streamlit as st
 
-url = 'https://hexivium-api-637a6012f8f4.herokuapp.com/calculate'
+class BirthDataCalculator:
+    def __init__(self, birth_date, birth_time, latitude, longitude):
+        self.url = 'https://hexivium-api-637a6012f8f4.herokuapp.com/calculate'
+        self.data = {
+            "birth_date": birth_date,
+            "birth_time": birth_time,
+            "latitude": latitude,
+            "longitude": longitude
+        }
 
-
-data = {
-    "birth_date": "06/10/1988",
-    "birth_time": "21:30",
-    "latitude": 6.0,
-    "longitude": 35.0
-}
-
-#Make the GET request
-response = requests.post(url, json=data)
-
-
-if response.status_code == 200:
-    # Print the response data
-    print("Response from server:", response.json())
-else:
-    # Print an error if something went wrong
-    print("Error:", response.status_code, response.text)
+    def calculate_birth_data(self):
+        try:
+            response = requests.post(self.url, json=self.data)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return {"error": f"Error: {response.status_code}, {response.text}"}
+        except Exception as e:
+            return {"error": f"An exception occurred: {str(e)}"}
