c92a33bede7f3c3c9865c5702f8d3d450ccf8d17
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-23 16:24:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,11 @@ with st.sidebar:
     birth_data = BirthDataCalculator(date, time, longitude, latitude)
     st.write(date, time)
     if st.button('Load data'):
-        content = birth_data.calculate_birth_data
-        st.write(content)
+        content = birth_data.calculate_birth_data()  # Call the method
+        if "error" in content:
+            st.write(f"Error: {content['error']}")
+        else:
+            st.write(content)
 
     st.divider()
     st.markdown("""<span ><font size=1>Connect With Me</font></span>""",unsafe_allow_html=True)
