0ba17a8a3eb321f954f34fde8cd31d0fab9239da
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-26 23:25:46 +0100

    Create word_list.py

---
+++ util/word_list.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def wordlist():
+    # Define your word list or obtain it from an external source
+  words = ["Personality", "Characteristics", "Character", "Attitude", "Behaviour", "Habits"]
+
+  
+  return words
