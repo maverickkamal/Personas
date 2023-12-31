c2a87274fafeec8794c632dcefad6ebbebc37b48
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-31 21:48:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ st.set_page_config(
 st.markdown('''# **ZENTiDE** - *Personality and Gene Keys*
 Zentide uniquely guides your path to self-growth and purpose by revealing AI-powered insights personalized to your DNA, 
 chronorhythm, and higher consciousness through an integrated framework of neural networks, ancient wisdoms and bio-algorithms.
+# **Note** 
+can't perform personal chat yet unless image is uploaded
 ''')
 
 #include(home=True)
