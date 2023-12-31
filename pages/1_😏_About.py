f39647ad92ebfff14f7356bd0afc980447f9d879
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-31 16:45:07 +0100

    Create 1_😏_About.py

---
+++ pages/1_😏_About.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.markdown('''# **Description**
+Zentide offers a gateway to self-actualization through harmonizing your inner consciousness with the cosmic dynamics that influence us all.
+Combining a foundation of mindful zen practices for presence with an overlay of your unique bio-rhythms and external cycles, Zentide provides tailored daily guidance to help you achieve inner peace and outward purpose.
+At Zentide's core sits an integrative framework powering personalized recommendations and insights generated from our proprietary fusion of neural networks, genetic algorithms, chronobiology research and ancient philosophies.
+The result? A pioneering path to discover your highest self – designed just for you each day by bridging cutting-edge AI capabilities with timeless intuitive wisdom.
+Zentide invites you on a beautiful journey of understanding your consciousness through the interplay of modern and ancient knowledge streams. Our intelligent platform nurtures self-growth and actualization within the greater context of natural rhythms all around and within us.
+# **Note**: It's still in development as the Personality Schema is still underway 
+''')
+
