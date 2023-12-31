628ff03cf9595ddf005e57754e6cc63c445741b1
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-31 21:32:54 +0100

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -4,7 +4,25 @@ from dotenv import load_dotenv
 #from typing import Sequence
 from io import BytesIO
 
-gemini_pro = GeminiMultiModal(model_name="models/gemini-pro-vision")
+safety_settings=[
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_ONLY_HIGH",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_ONLY_HIGH",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_ONLY_HIGH",
+    }
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_ONLY_HIGH",
+    },
+]
+gemini_pro = GeminiMultiModal(model_name="models/gemini-pro-vision", safety_settings=safety_settings)
 class ImageDocument:
     def __init__(self, image_data):
         self.image_data = image_data
@@ -20,6 +38,7 @@ def gemini(prompt, image_data):
         image_documents=[images_document],
     )
     # Use a list to store parts of the response
+    
     response_parts = []
     
     for r in stream_complete_response:
