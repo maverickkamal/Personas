b4ce0f65bea2c2a6b79f651abea521be4937f84a
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2023-12-31 16:37:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,20 @@ def load_data():
         st.write(f"Error: {content['error']}")
 #global content
 with st.sidebar:
-    st.write("Provide your Personas")
+    st.markdown('''# **Description**
+    Zentide offers a gateway to self-actualization through harmonizing your inner consciousness with the cosmic dynamics that influence us all.
+
+Combining a foundation of mindful zen practices for presence with an overlay of your unique bio-rhythms and external cycles, Zentide provides tailored daily guidance to help you achieve inner peace and outward purpose.
+
+At Zentide's core sits an integrative framework powering personalized recommendations and insights generated from our proprietary fusion of neural networks, genetic algorithms, chronobiology research and ancient philosophies.
+
+The result? A pioneering path to discover your highest self – designed just for you each day by bridging cutting-edge AI capabilities with timeless intuitive wisdom.
+
+Zentide invites you on a beautiful journey of understanding your consciousness through the interplay of modern and ancient knowledge streams. Our intelligent platform nurtures self-growth and actualization within the greater context of natural rhythms all around and within us.
+    # **Note**: It's still in development as the Personality Schema is still underway 
+
+    ''')
+    st.write("# Provide your Personas")
     stdate = st.date_input("Enter date of Birth", value="today", min_value=minDate, max_value=maxDate, format="DD/MM/YYYY")
     sttime = st.sidebar.time_input("Enter time of birth", datetime.time(12, 00))
     longitude = st.number_input("Insert your longitude", value=15, placeholder="Type your longitude...")
